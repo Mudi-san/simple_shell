d176638503a62e081aa5fd2b8818ae2196636a39
Mudi-san <mudijewilly@gmail.com>
2023-08-23 07:59:08 -0700

    mudi added

--- h_str.c
+++ h_str.c
@@ -1,132 +1,74 @@
 #include "hssh.h"
 
 /**
- * _atoi - converts string to integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string or -1 on error or converted number
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
  */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+
 int _atoi(char *s)
 {
-	int i = 0;
-	unsigned long int result = 0;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-	if (*s == '+')
-		s++;
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
+		if (s[i] == '-')
+			sign *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+			flag = 1;
 			result *= 10;
 			result += (s[i] - '0');
-			if (result > INT_MAX)
-				return (-1);
 		}
-		else
-			return (-1);
+		else if (flag == 1)
+			flag = 2;
 	}
-	return (result);
-}
 
-/**
- * print_error - print error message
- * @data: parameter and return info struct
- * @estr: string contain error type
- * Return: 0 if no numbers in string or -1 on error or converted number
- */
-void print_error(esh_t *data, char *estr)
-{
-	_puts(data->esh_name);
-	_puts(": ");
-	print_decimal(data->line_count);
-	_puts(": ");
-	_puts(data->argv[0]);
-	_puts(": ");
-	_puts(estr);
-}
-
-/**
- * print_decimal - function prints base 10 numbers
- * @input: input
- *
- * Return: the number of characters that be printed
- */
-int print_decimal(int input)
-{
-	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
-
-	if (input < 0)
-	{
-		_abs_ = -input;
-		__putchar('-');
-		count++;
-	}
+	if (sign == -1)
+		output = -result;
 	else
-		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
+		output = result;
 
-	return (count);
-}
-
-/**
- * convert_number - converter function use in atoi function
- * @num: number
- * @base: base
- *
- * Return: string
- */
-char *convert_number(long int num, int base)
-{
-	static char *array;
-	static char buffer[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = num;
-
-	if (num < 0)
-	{
-		n = -num;
-		sign = '-';
-	}
-	array = "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-
-	do {
-		*--ptr = array[n % base];
-		n /= base;
-	} while (n != 0);
-
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
-}
-
-/**
- * remove_comments - replaces first instance of '#' with '\0'
- * @str: address of the string
- * Return: 0;
- */
-void remove_comments(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		if (str[i] == '#' && (!i || str[i - 1] == ' '))
-		{
-			str[i] = '\0';
-			break;
-		}
+	return (output);
 }
